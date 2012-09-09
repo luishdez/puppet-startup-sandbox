a9fd889451b0e90ffef1616a33a3036fb4e634b7
Luis Muñoz Hernández <luis.munoz.hdez@gmail.com>
2012-09-09 12:10:46 +0200

    Add support for notify in bind

--- manifests/node.dns.pp
+++ manifests/node.dns.pp
@@ -7,7 +7,15 @@ node 'default' {
   include basic
   
   bind::setup {'bind_setup':
-    firewall => 'iptables'
+    firewall => 'iptables',
+    customConf => {
+      'options' => {
+        'listen-on port 53' => {'any' => '','192.168.100.200' => '','192.168.100.201' => ''},
+        'allow-transfer'    => {'any' => '','192.168.100.200' => '','192.168.100.201' => ''},
+        'also-notify'       => {'192.168.100.201' => ''}, 
+        'allow-notify'      => {'192.168.100.201' => ''} 
+      }
+    }
   }
   
   webmin::setup {'webmin_setup':
